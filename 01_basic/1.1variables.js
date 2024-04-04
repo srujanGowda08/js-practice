649f4f45defa54f8f5c32b7b1affa9603bfdbd4a
srujanGowda08 <srujangamer08@gmail.com>
2024-04-04 23:41:40 +0530

    1st day

---
+++ 01_basic/1.1variables.js
@@ -0,0 +1,23 @@
+const accountId = 12346;
+
+let accountEmail = "srujan08@gmail.com";//we can change the value
+
+var accountPassword = "12345";//we can change the value
+
+accountCity = "Bangalore";//we can change the value
+
+let accountState;//undefined 
+
+
+// accountId =11;(we cannot change the value of variable)
+
+accountEmail = "sruja@gmail.com" ;
+accontPassword = "1222";
+accontCity = "Mysore";
+
+console.log(accountId);
+
+console.table([accountEmail,accontPassword,accontCity]);
+
+/* Prefer not to use var keyword 
+because of issue in block scope and functional scope */
---
+++ 01_basic/1.2dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version of ECMAScript.
+
+//alert('Hello, world!');  we are using nodejs not browser js engine.
+
+let name ="srujan";
+let age = 25;
+let loggedIn = true;
+
+//number => 2 to the power 53
+//bigint
+//string => ""
+//bollean => true/false
+//null => standalone value 
+//undefined => no value assigned to it.
+//symbol => unique identifier for a particular object
+
+
+//objects are dynamic and flexible data structures.
+
+console.log(typeof "srujan");
+console.log(typeof null);//type of null is object.
+
